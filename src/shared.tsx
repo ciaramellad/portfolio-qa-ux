f33ad669a929a8d10a08a708f4ddb65c76b0b65d
Daiana <103391862+ciaramellad@users.noreply.github.com>
2026-03-27 16:52:19 +0100

    Update shared.tsx

--- src/shared.tsx
+++ src/shared.tsx
@@ -192,7 +192,7 @@ export const Header = ({ theme, toggleTheme, lang, toggleLang, t }: { theme: 'da
         ))}
         <div className="mt-4 border-t border-current/10 pt-8 w-full">
           <a
-            href="public/CVDaianaCiaramella-QA.pdf"
+            href="/img/CVDaianaCiaramella-QA.pdf"
             download
             className={`flex items-center gap-3 text-xl md:text-2xl lg:text-3xl hover:opacity-50 transition-opacity font-light tracking-tighter`}
           >
